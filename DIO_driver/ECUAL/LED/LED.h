f09525405ec2b5cbcd8445210ce6e4b98b3f0207
Kariman Karam <39091696+Kariman95@users.noreply.github.com>
2021-07-09 18:39:54 +0200

    Add files via upload

---
+++ DIO_driver/ECUAL/LED/LED.h
@@ -0,0 +1,51 @@
+/*
+ * LED.h
+ *
+ *  Created on: Jul 9, 2021
+ *      Author: Kariman
+ */
+
+#ifndef ECUAL_LED_LED_H_
+#define ECUAL_LED_LED_H_
+
+/*- INCLUDES -----------------------------------------------*/
+#include "../../LIB/BIT_math.h"
+#include "../../LIB/BIT_math.h"
+#include "../../MCAL/DIO/DIO_cfg.h"
+#include "../../MCAL/DIO/DIO_init.h"
+
+/*- FUNCTION DECLARATIONS ----------------------------------*/
+
+/*************************************************************************************************
+* Parameters (in) : None
+* Parameters (out): None
+* Return Value    : enuErrorStatus_t - For error handling if the user input an invalid parameter
+* Description     : A function to initialize the led
+*************************************************************************************************/
+enuErrorStatus_t	LED_Init(void);
+
+/*************************************************************************************************
+* Parameters (in) : u8_LedNum(index to the led pin)
+* Parameters (out): None
+* Return Value    : enuErrorStatus_t - For error handling if the user input an invalid parameter
+* Description     : A function to turn the led on
+*************************************************************************************************/
+enuErrorStatus_t	LED_ON(uint8_t u8LedNum);
+
+/*************************************************************************************************
+* Parameters (in) : u8_LedNum(index to the led pin)
+* Parameters (out): None
+* Return Value    : enuErrorStatus_t - For error handling if the user input an invalid parameter
+* Description     : A function to turn the led off
+*************************************************************************************************/
+enuErrorStatus_t	LED_OFF(uint8_t u8LedNum);
+
+/*************************************************************************************************
+* Parameters (in) : u8_LedNum(index to the led pin)
+* Parameters (out): None
+* Return Value    : enuErrorStatus_t - For error handling if the user input an invalid parameter
+* Description     : A function to toggle the led
+*************************************************************************************************/
+enuErrorStatus_t	LED_toggle(uint8_t u8LedNum);
+
+#endif /* ECUAL_LED_LED_H_ */
